9e8110bf5359cac297b667e481defaae042916cb
petrosTsatsis <tsatsis.pe@gmail.com>
2023-12-11 17:07:08 +0200

    Added scoreboard and wall collision.

--- food.py
+++ food.py
@@ -17,6 +17,6 @@ class Food(Turtle):
         self.refresh()
 
     def refresh(self):
-        random_x = random.randint(-320, 320)
-        random_y = random.randint(-320, 320)
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
         self.goto(random_x, random_y)
