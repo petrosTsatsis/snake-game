7258777af593e238e87ea5db4186980ceafd9940
petrosTsatsis <tsatsis.pe@gmail.com>
2023-12-11 17:17:52 +0200

    Added snake extend and tail collision.

--- main.py
+++ main.py
@@ -24,17 +24,24 @@ screen.onkey(snake.right, "Right")
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(0.05)
     snake.move()
 
     # Detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
 
     # Detect collision with wall.
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head .ycor() > 280 or snake .head.ycor() < -280:
         game_is_on = False
         scoreboard.game_over()
+
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
